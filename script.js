9e1b3384919bdc00a3d5bb19eb8549dc6d69b5b5
Giovanna Alves Do Nascimento <134456621+GiovannaNas@users.noreply.github.com>
2024-02-26 16:50:48 -0300

    updates
    updates

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar2.png")
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar1.png")
   }
 }
